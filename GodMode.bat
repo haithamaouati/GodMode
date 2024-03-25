10a7c4721447d460a99598841c7278749afa24d3
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2024-03-25 17:35:01 +0100

    Update GodMode.bat

--- GodMode.bat
+++ GodMode.bat
@@ -8,8 +8,52 @@ REM GitHub: github.com/haithamaouati
 
 cls
 
+net session >nul 2>&1
+if %errorlevel% == 0 (
+    goto main_menu
+) else (
+    echo This script must be run as an administrator.
+    pause
+    exit
+)
+
+:main_menu
+cls
+echo.
+echo  ##########  ##########
+echo  ##########  ##########
+echo  ##########  ##########
+echo  ##########  ##########
+echo  ##########  ##########
+echo.
+echo  ##########  ##########
+echo  ##########  ##########
+echo  ##########  ##########
+echo  ##########  ##########
+echo  ##########  ##########
+echo.
+echo Main menu:
+echo 1. God Mode
+echo 2. Exit
+echo.
+
+set /p choice=Enter your choice: 
+
+if "%choice%"=="1" goto godmode
+if "%choice%"=="2" goto end
+
+echo Invalid choice. Please try again.
+goto main_menu
+
+:godmode
 set folderName="GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}"
 mkdir %folderName%
-echo
 pause
-exit
+goto main_menu
+
+:end
+echo Are you sure you want to exit the script? (y/n)
+set /p confirm=
+if /i "%confirm%"=="y" exit
+if /i "%confirm%"=="n" goto main_menu
+goto end
