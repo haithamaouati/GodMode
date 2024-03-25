9c3fab9c32f3c5d5bf55b181b55e9f3940d74a31
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2024-03-25 16:53:22 +0100

    Create GodMode.bat

---
+++ GodMode.bat
@@ -0,0 +1,15 @@
+@echo off
+title GodMode
+mode 80,25
+color 07
+
+REM Author: Haitham Aouati
+REM GitHub: github.com/haithamaouati
+
+cls
+
+set folderName="GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}"
+mkdir %folderName%
+echo
+pause
+exit
